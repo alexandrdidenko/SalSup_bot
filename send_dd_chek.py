f0b1c0f67f407dbdc15491c5af66617f0f04a54a
alexandrdidenko <alekszender@gmail.com>
2020-06-17 18:44:00 +0300

    Добавил проверку точек ДД отправляет мне

--- send_dd_chek.py
+++ send_dd_chek.py
@@ -4,5 +4,5 @@ import task
 text = 'Статус: точки DD'
 
 task.run_macro(config.xls_DD_chek, config.macros_DD_chek)
-task.send(config.ID_SALESSUPPORT_CHANNEL, config.photo_DD_chek, text, __file__)
+task.send(config.id_bot_and_my, config.photo_DD_chek, text, __file__)
 task.del_fife(config.photo_DD_chek)
